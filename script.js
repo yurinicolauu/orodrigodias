e4408e507813882f48ebcaadf7488bb4281cbba0
Yuri Nicolau <ncl.yuri@gmail.com>
2023-06-28 21:15:10 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem, se tiver light mode, adicionar a imagem light, se tiver sem light mode, manter a imagem normal
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
